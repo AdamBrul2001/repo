06148da219c7c44284a7cabfd05d0401a554c584
AdamBrul2001 <adam.brulinski@psr.gda.pl>
2020-12-07 15:55:04 +0100

    07.12.2020

---
+++ src/Cards/Main.java
@@ -0,0 +1,16 @@
+package Cards;
+
+public class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+
+        for(Card c: deck.deck) {
+            System.out.println(c);
+        }
+
+        System.out.println(deck.deck.size());
+        Card card = deck.draw();
+        System.out.println(deck.deck.size());
+        System.out.println(card);
+    }
+}
